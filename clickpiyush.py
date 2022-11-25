f307e35da4010fdaa88c63c1a106bdb7a5cc338c
Piyush <thesourcepedia@gmail.com>
2022-11-25 13:25:42 +0530

    feat: add prompt option

--- clickpiyush.py
+++ clickpiyush.py
@@ -7,17 +7,20 @@ def cli():
 
 
 @click.command()
+@click.option('--name', prompt='Identify youself by name')
+def greet(name):
+    click.echo(
+        f"Hello {name}! This is Command Line Interface which gives information of maker named Piyush.")
+
+
+@click.command()
 @click.option('--desc', default=0, help='Detailed Info.')
 def bio(desc):
     click.echo("This is me, and describing self.")
 
 
-@click.command()
-@click.argument('name')
-def greet(name):
-    click.echo(f"Hello {name}! I'm Piyush.")
-
-
+# @click.command()
+# @click.Choice()
 cli.add_command(greet)
 cli.add_command(bio)
 
