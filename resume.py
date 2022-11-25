654832c68bb0cd44e755851c1fa87dcbd1f43c22
Piyush <thesourcepedia@gmail.com>
2022-11-25 12:10:24 +0530

    init: command group added

--- resume.py
+++ resume.py
@@ -1,10 +1,24 @@
 import click
 
 
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option('--desc', default=0, help='Detailed Info.')
+def bio(desc):
+    click.echo("This is me, and describing self.")
+
+
 @click.command()
 def greet():
     click.echo("Hello! I'm Piyush.")
 
 
+cli.add_command(greet)
+cli.add_command(bio)
+
 if __name__ == '__main__':
-    greet()
+    cli()
